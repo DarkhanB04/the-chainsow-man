a766ac70639917062379c82eba72efde73039f11
Darkhan B <darkhanbekuzak13@gmail.com>
2024-10-22 00:27:18 +0500

    Added new version with updates

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const soundBtn = document.querySelector(".sound-button");
+let isPlayed = false;
+var audio = new Audio("audio/song.mp3");
+
+soundBtn.addEventListener("click", playAudio);
+function playAudio() {
+  if (isPlayed) {
+    audio.pause();
+    soundBtn.innerText = "Ow Ow";
+
+    return (isPlayed = false);
+  }
+  isPlayed = true;
+
+  audio.play();
+  soundBtn.innerText = "Stop";
+}
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const releaseTime = new Date("2022-10-22T00:00:00");
+function updateTime() {
+  const currentTime = new Date();
+
+  const diff = currentTime - releaseTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+setInterval(updateTime, 1000);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  freeMode: true,
+  autoplay: {
+    delay: 3300,
+    disableOnInteraction: false,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    270: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    430: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
